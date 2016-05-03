5c4d09e70a330cb3f68c8b20ab3f148a6e9bc734
Christina Jensen <cj.christina.jensen@gmail.com>
2016-05-02 17:02:18 -0700

    creating red/black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+window.onload = () => {
+  let body = document.querySelector('body');
+  body.setAttribute('style', 'margin: 0;');
+  
+  for(var i = 0; i < 45; i++) { // refactor to non-hardcoded maximum
+    let div = document.createElement('div');
+      if(i % 2 === 0) {
+        div.setAttribute('style', 'width: 11.1%; float: left; padding-bottom: 11.1%; background-color: black;');
+      } else {
+        div.setAttribute('style', 'width: 11.1%; float: left; padding-bottom: 11.1%; background-color: red;');
+      }
+    body.appendChild(div);
+  }
+
+}
